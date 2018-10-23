fb8febd9d498f98b64950c13e24e517fc9422dcc
James Horton <jameshorton@gmail.com>
2018-10-23 15:58:03 -0400

    updates are sent to server. only new updates are sent back to client. updates do not show up on page yet.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express")
 const port = 3000
 const app = express()
+const updates = [];
 
 app.use(express.static('public'))
 app.use(express.json())
 
 // Fill in your request handlers here
+
+app.post("/updates", function (req, res){
+    if(req.body.clientUpdates.length > 0) {updates.push(req.body.clientUpdates)}
+    console.log(req.body.lengthOfLastServerUpdate)
+    newUpdates = updates.slice(req.body.lengthOfLastServerUpdate)
+    res.send({
+        "serverUpdates": newUpdates
+    });
+})
 
 app.listen(port)
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,27 @@
 // Add logic to this script to poll server every second for updated pixels.
+
+const timeoutLength = 1000;
+let lengthOfLastServerUpdate = 0;
+
+const pollServer = async function () {
+    let body = {
+        "clientUpdates": clientUpdates,
+        "lengthOfLastServerUpdate": lengthOfLastServerUpdate
+    }
+    body = JSON.stringify(body);
+    clientUpdates = [];
+    let res = await fetch('/updates', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: body
+    })
+    res = await res.json();
+    lengthOfLastServerUpdate += res.serverUpdates.length
+    console.log("lengthOfLastServerUpdate", lengthOfLastServerUpdate)
+    console.log(res.serverUpdates)
+
+    setTimeout(pollServer, timeoutLength);
+};
+setTimeout(pollServer, timeoutLength);
